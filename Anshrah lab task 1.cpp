bce30dc943388b54c5e5fd6c9b1eb87f7304b8a9
anshrah-fahim <anshrahfahim1@gmail.com>
2024-12-04 10:15:35 +0500

    Add files via upload

---
+++ Anshrah lab task 1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+	
+	string str1 = "Hello,";
+	string str2 = "World!";
+	string result = str1.append(str2);
+	cout<<result;
+	
+	return 0;
+}
---
+++ Anshrah lab task 2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+	
+	string str = "Hello, World!";
+	int length = str.length();
+	cout<<"The length of the string is : "<<length<<endl;
+	
+	return 0;
+}
